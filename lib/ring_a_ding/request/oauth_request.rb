024bbcc42b2dc07284496cbca2cec4fabdb27fe4
Maxime Delpit <maximedelpit@gmail.com>
2018-07-19 13:08:34 +0200

    Intuition for OauthRequest

--- lib/ring_a_ding/request/oauth_request.rb
+++ lib/ring_a_ding/request/oauth_request.rb
@@ -1,10 +1,12 @@
+require 'securerandom'
+
 module RingADing
   class OauthRequest < Request
 
     # https://ssl.keyyo.com/oauth2/authorize.php
     # https://api.keyyo.com/oauth2/token.php
     #  CAREFUL 2 different API URL for authorize & token!
-    BASE_API_URL = nil #'https://ssl.keyyo.com/oauth2/'
+    BASE_API_URL = 'RANDOM SHIT TO BE REPLACED' #'https://ssl.keyyo.com/oauth2/'
     OAUTH_URLS = {
       authorize_url: 'https://ssl.keyyo.com/oauth2/',
       token_url: 'https://api.keyyo.com/oauth2/token.php',
@@ -24,9 +26,21 @@ module RingADing
         state: generate_state,
         client_id: @client.api_key
       }
-      result = retrieve(params: params)
+      begin
+        result = retrieve(params: params)
       # NB / REFACTO: should handle refesh there ?
-      return result
+        return result
+      rescue => e
+        puts "ERROR ===> #{e}"
+        binding.pry
+        # CODE EXAMPLE !!!
+        # redirect_uri = 'http://localhost:3000/users/auth/keyyo/callback'
+        # client = RingADing::Client.new(auth_type: 'Bearer')
+        # ring_a_ding = RingADing::OauthRequest.new(client)
+        # ring_a_ding.authorize(redirect_uri)
+        # binding.pry
+        # # Redirect issue => how to redirect to keyyo  plain ruby instead of Response ?
+      end
     end
 
     def get_token(code, state)
@@ -34,6 +48,12 @@ module RingADing
       body.merge!({code: code, state: state})
       result = create(body: body)
       return result
+      # CODE EXAMPLE !!!
+      # client = RingADing::Client.new(auth_type: 'Bearer')
+      # ring_a_ding = RingADing::OauthRequest.new(client)
+      # code = 'todo'
+      # code = 'state'
+      # get_token(code, state)
     end
 
     def refresh_token(refresh_token, redirect_uri)
@@ -46,7 +66,7 @@ module RingADing
     private
 
     def generate_state
-      return '123'
+      return SecureRandom.uuid
     end
 
     def prepare_token_query(base_url, part_method_name, grant_type)
